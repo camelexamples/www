1a0e0a7bd51ad6f160891122c4cfb61590d15d0c
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2022-02-16 21:47:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
 echo "test"
+
+$table = ["key1"=>[1,2,3,4],
+                 "key2"=>[2,3,4,5],
+                 "key3"=>[3,4,5,6],
+                 "key4"=>[4,5,6,7]
+           ];
+
+$out  = "";
+$out .= "<table>";
+foreach($table as $key => $element){
+    $out .= "<tr>";
+    foreach($element as $subkey => $subelement){
+        $out .= "<td>$subelement</td>";
+    }
+    $out .= "</tr>";
+}
+$out .= "</table>";
+
+echo $out;
